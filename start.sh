5998addfe0963866df1a852c895fce15129ac402
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-11 07:02:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Your_Repo_Name /LazyDeveloper
+git clone https://github.com/konichiwa55115/TG-WhatsApp /LazyDeveloper
 cd /LazyDeveloper
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt & npm install
 echo "Starting Bot...."
 npm start
