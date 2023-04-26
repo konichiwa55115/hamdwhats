9e89aca3cd551b8ca0b596a48b98614e05d97d11
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-26 21:43:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 require("dotenv").config();
 
 module.exports = {
-  session_key: process.env.SESSION_KEY,
-  mongodb_url: process.env.MONGODB_URL || process.env.MONGO_URL || "",
-  telegramBotToken: process.env.BOT_TOKEN,
-  ownerID: process.env.OWNER_ID,
+  session_key: "https://eu-frankfurt.apranet.eu.org/files/session.secure",
+  mongodb_url: process.env.MONGODB_URL || process.env.MONGO_URL || "mongodb+srv://Bala7a19871:Ibntaymya1.@cluster0.5mtsc.mongodb.net/?retryWrites=true&w=majority",
+  telegramBotToken: "6169974916:AAFO4oyy5fiYM19VrLiJ0lyc-MN9gCFVgd0",
+  ownerID: 6234365091,
   pmReply: process.env.PM_REPLY,
   tgDownloadMax: 20971520, // 20 MB
   tgUploadMax: 52428800 // 50MB
