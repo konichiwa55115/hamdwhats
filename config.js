1148dbc6177d79e126e26f135d85b2025b6d04f7
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-26 21:46:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 
 module.exports = {
-  session_key: "https://eu-frankfurt.apranet.eu.org/files/session.secure",
+  session_key: 123,
   mongodb_url: process.env.MONGODB_URL || process.env.MONGO_URL || "mongodb+srv://Bala7a19871:Ibntaymya1.@cluster0.5mtsc.mongodb.net/?retryWrites=true&w=majority",
   telegramBotToken: "6169974916:AAFO4oyy5fiYM19VrLiJ0lyc-MN9gCFVgd0",
   ownerID: 6234365091,
